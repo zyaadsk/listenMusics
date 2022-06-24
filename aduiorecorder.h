a72431b85cc43cd8adf7bf6b1e3a0317a4d54525
Yggdrasill <2247972470@qq.com>
2022-06-24 19:47:40 +0800

    ver1.3

---
+++ aduiorecorder.h
@@ -0,0 +1,15 @@
+#ifndef ADUIORECORDER_H
+#define ADUIORECORDER_H
+
+#include <QWidget>
+
+class aduiorecorder : public QWidget
+{
+public:
+    explicit aduiorecorder(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // ADUIORECORDER_H
